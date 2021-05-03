f4476b84cc3714f4a8a75c00f40d60459d7f36ae
Bartosz Zalewski <80588334+Deszynski@users.noreply.github.com>
2021-05-03 18:51:02 +0200

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿
+namespace Grafika_Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.suwak1 = new System.Windows.Forms.TrackBar();
+            this.btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.pic = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.suwak1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // suwak1
+            // 
+            this.suwak1.Location = new System.Drawing.Point(274, 74);
+            this.suwak1.Name = "suwak1";
+            this.suwak1.Size = new System.Drawing.Size(302, 56);
+            this.suwak1.TabIndex = 0;
+            this.suwak1.Scroll += new System.EventHandler(this.suwak1_Scroll);
+            // 
+            // btn
+            // 
+            this.btn.Location = new System.Drawing.Point(12, 12);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(148, 35);
+            this.btn.TabIndex = 4;
+            this.btn.Text = "Załaduj obraz";
+            this.btn.UseVisualStyleBackColor = true;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(1016, 74);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 154);
+            this.panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Location = new System.Drawing.Point(1016, 302);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(256, 154);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel3.Location = new System.Drawing.Point(1016, 523);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(256, 154);
+            this.panel3.TabIndex = 0;
+            // 
+            // pic
+            // 
+            this.pic.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pic.Location = new System.Drawing.Point(39, 147);
+            this.pic.Name = "pic";
+            this.pic.Size = new System.Drawing.Size(909, 574);
+            this.pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic.TabIndex = 0;
+            this.pic.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(401, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "kontrast";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1137, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 17);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "R";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1137, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(19, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "G";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(1139, 503);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 17);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "B";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1321, 747);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pic);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.suwak1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.suwak1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TrackBar suwak1;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.PictureBox pic;
+        private System.Windows.Forms.Label label3;
+        public System.Windows.Forms.Panel panel1;
+        public System.Windows.Forms.Panel panel2;
+        public System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grafika_Lab6
+{
+    public partial class Form1 : Form
+    {
+         Bitmap bitmap;
+
+        int[] histR = new int[256];
+        int[] histG = new int[256];
+        int[] histB = new int[256];
+
+        bool read = false;
+    
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void suwak1_Scroll(object sender, EventArgs e)
+        {
+            Bitmap bitmapEdited = (Bitmap)bitmap.Clone();
+
+
+            int width = pic.Image.Width;
+            int height = pic.Image.Height;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+
+                    Color cA = bitmapEdited.GetPixel(x, y);
+
+                    int r = cA.R;
+                    int g = cA.G;
+                    int b = cA.B;
+
+
+                    // R
+                    if (r < 127 + suwak1.Value)
+                    {
+                        r = (127 / (127 + suwak1.Value)) * r;
+                    }
+                    else if (r > 127 - suwak1.Value)
+                    {
+                        r = (127 * r + 255 * suwak1.Value) / (127 + suwak1.Value);
+                    }
+                    else
+                    {
+                        r = 127;
+                    }
+
+                    // G
+                    if (g < 127 + suwak1.Value)
+                    {
+                        g = (127 / (127 + suwak1.Value)) * g;
+                    }
+                    else if (g > 127 - suwak1.Value)
+                    {
+                        g = (127 * g + 255 * suwak1.Value) / (127 + suwak1.Value);
+                    }
+                    else
+                    {
+                        g = 127;
+                    }
+
+                    // B
+                    if (b < 127 + suwak1.Value)
+                    {
+                        b = (127 / (127 + suwak1.Value)) * b;
+                    }
+                    else if (b > 127 - suwak1.Value)
+                    {
+                        b = (127 * b + 255 * suwak1.Value) / (127 + suwak1.Value);
+                    }
+                    else
+                    {
+                        b = 127;
+                    }
+
+                    bitmapEdited.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+
+            pic.Image = bitmapEdited;
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog Opfile = new OpenFileDialog();
+            if (DialogResult.OK == Opfile.ShowDialog())
+            {
+                this.pic.Image = new Bitmap(Opfile.FileName);
+                bitmap = (Bitmap)this.pic.Image;
+
+                Array.Clear(histR, 0, histR.Length);
+                Array.Clear(histG, 0, histG.Length);
+                Array.Clear(histB, 0, histB.Length);
+
+
+                histogram();
+
+                read = true;
+
+                panel1.Invalidate();
+                panel2.Invalidate();
+                panel3.Invalidate();
+                panel1.Paint += new PaintEventHandler(panel1_Paint);
+                panel2.Paint += new PaintEventHandler(panel2_Paint);
+                panel3.Paint += new PaintEventHandler(panel3_Paint);
+                suwak1.Value = 0;
+            }
+        }
+
+        private void histogram()
+        {
+            int width = pic.Image.Width;
+            int height = pic.Image.Height;
+
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    Color cA = bitmap.GetPixel(x, y);
+
+                    int r = cA.R;
+                    int g = cA.G;
+                    int b = cA.B;
+
+                    histR[r]++;
+                    histG[g]++;
+                    histB[b]++;
+                }
+            }
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            if (read == true)
+            {
+                Graphics graphR = e.Graphics;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    float r = histR[i];
+
+                    r = r / (pic.Image.Height * pic.Image.Width);
+                    r *= 6000;
+
+                    graphR.DrawLine(new Pen(Color.Red), i, panel1.Height, i, panel1.Height - r);
+                }
+            }
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+            if (read == true)
+            {
+                Graphics graphR = e.Graphics;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    float r = histG[i];
+
+                    r = r / (pic.Image.Height * pic.Image.Width);
+                    r *= 6000;
+
+                    graphR.DrawLine(new Pen(Color.Green), i, panel1.Height, i, panel1.Height - r);
+                }
+            }
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+            if (read == true)
+            {
+                Graphics graphR = e.Graphics;
+
+                for (int i = 0; i < 256; i++)
+                {
+                    float r = histB[i];
+
+                    r = r / (pic.Image.Height * pic.Image.Width);
+                    r *= 6000;
+
+                    graphR.DrawLine(new Pen(Color.Blue), i, panel1.Height, i, panel1.Height - r);
+                }
+            }
+        }
+    }
+}
